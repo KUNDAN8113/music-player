da403bb9b44facb43e6a8abd2790cddd278c3512
KUNDAN8113 <kundansmith36@gmail.com>
2022-03-24 17:29:18 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState, useEffect} from 'react';
+import Player from './components/Player';
 
 function App() {
+  const [songs] = useState([
+    {
+      title:"Dil ibadat",
+      artist:"KK",
+      img_src:"./images/kk.jpg",
+      src:"./music/Dil-ibadat.mp3"
+
+    },
+    {
+      title:"Sawali si raat",
+      artist:"Arijit singh",
+      img_src:"./images/arijit.jpg",
+      src:"./music/sawali-si-raat.mp3"
+    },
+    {
+      title:"Ik tu hai",
+      artist:"Jubin Nautiyal",
+      img_src:"./images/jubin.jpg",
+      src:"./music/ik-tu-hai.mp3"
+    },
+    {
+      title:"Tera hone laga hoon",
+      artist:"Atif Aslam",
+      img_src:"./images/atif.jpg",
+      src:"./music/tera-hone.mp3"
+    },
+    {
+      title:"Rehnuma",
+      artist:"Shreya Ghosal",
+      img_src:"./images/shreya.jpg",
+      src:"./music/rehnuma.mp3"
+    },
+    {
+      title:"Halka halka",
+      artist:"Sunidhi chauhan",
+      img_src:"./images/sunidhi.jpg",
+      src:"./music/halka-halka.mp3"
+    },
+    {
+      title:"Hai dil ye mera",
+      artist:"Arijit singh",
+      img_src:"./images/arijit.jpg",
+      src:"./music/hai-dil.mp3"
+    },
+    {
+      title:"Chura liya",
+      artist:"Sachet & Parampara",
+      img_src:"./images/sachet.jpg",
+      src:"./music/chura-liya.mp3"
+    }
+  ]);
+
+  const [currentSongIndex, setCurrentSongIndex] = useState(0);
+  const [nextSongIndex, setNextSongIndex] = useState(0);
+
+  useEffect(() => {
+    setNextSongIndex(() => {
+      if (currentSongIndex + 1 > songs.length - 1) {
+        return 0;
+      } else {
+        return currentSongIndex + 1;
+      }
+    });
+  }, [currentSongIndex, songs.length]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Player 
+        currentSongIndex={currentSongIndex} 
+        setCurrentSongIndex={setCurrentSongIndex} 
+        nextSongIndex={nextSongIndex} 
+        songs={songs}
+      />
     </div>
   );
 }
